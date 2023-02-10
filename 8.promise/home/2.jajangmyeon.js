c45d3696495076e9347b6b00ee92e81b00f1aa6c
Han1234 <a@a.com>
2023-02-11 01:19:49 +0900

    1

--- 8.promise/home/2.jajangmyeon.js
+++ 8.promise/home/2.jajangmyeon.js
@@ -6,34 +6,33 @@ class Food {
         this.taste = taste
         this.foodName = foodName
     }
-
-    [Symbol.toPrimitive]() {
-        return `${this.taste} ${this.foodName}.`
-    }
 }
 
 class Chef {
+    constructor(taste) {
+        this.taste = taste
+    }
+
     async cook(foodName) {
         let promise = new Promise((resolve, reject) =>
-        setTimeout(() => resolve(new Food(taste, foodName)), 1000) ||
-                        reject('영업종료'))
+            setTimeout(() => resolve(new Food(this.taste, foodName)), 1000))
         let result = await promise
         return result
     }
 }
 
 class Waiter { //손님과 waiter사이의 Promise가 return.
-    async order() {
-        return await Promise.resolve(new Chef.cook)
+    async order(foodName, chef) {
+        return await Promise.resolve(chef.cook(foodName))
     }
 }
 
 const chef1 = new Chef('달콤한')
 const chef2 = new Chef('매운')
-const waiter = new Waiter().order().then(console.log)
+const waiter = new Waiter()
 
 waiter.order('짜장면', chef1)
 waiter.order('짜장면', chef2)
 waiter.order('짬뽕', chef2)
 
-console.log(waiter.order('짬뽕', chef2))
+waiter.order('짬뽕', chef2).then(console.log)
