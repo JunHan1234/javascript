5cb5ede21187e1b31dac886e682a7521872e3670
Han1234 <a@a.com>
2023-02-10 15:27:41 +0900

    1

--- 8.promise/1.basic.js
+++ 8.promise/1.basic.js
@@ -1,7 +1,7 @@
 //Promise(executor) // executor는 callback이다.
 //node가 넣어줄 callback parameter 2개를 기술한다.
- //실행되면, Promise객체의 state의 value가 resolved로 바뀌고, 
- //'done'은 Promise객체의 result의 value가 된다.
+ //실행되면, promise객체의 property인 state의 value가 resolved로 바뀌고, 
+ //'done'은 promise객체의 result의 value가 된다.
 let promise = new Promise((resolve, reject) => resolve('done'))
 
 //resolve에 들어갈 method, reject에 들어갈 method 준비.
@@ -9,7 +9,7 @@ let promise = new Promise((resolve, reject) => resolve('done'))
 //promise.then(console.log, console.log)
 //promise.then(console.log, null)
 promise.then(console.log)//resolve handler만 등록하고 끝내도 된다.
-promise.catch(console.log)//reject handler만 등록하고 싶을때 쓴다.
+promise.catch(console.log)//reject handler만 등록하고 싶을땐 catch를 쓴다.
 //실행된 후, promise 객체의 property인 result의 value가 return된다.
 
 // 과제: 1초 뒤에, resolve 하라
@@ -20,5 +20,5 @@ promise.then(console.log, console.log)
 //
 promise = new Promise((resolve, reject) => 
     setTimeout(() => reject(new Error('error')), 2000)) //'error'가 result의 value가 된다.
-promise.then(result => console.log(1, result),
-            result => console.log(2, result)) //resolve handler, error handler 준비.
+promise.then(result => console.log(1, result),/*resolve handler가 실행될 경우 실행할 method.*/
+            result => console.log(2, result))/*error handler가 실행될 경우 실행할 method.*/
